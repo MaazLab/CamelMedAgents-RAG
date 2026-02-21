d1c5cc22e81de38a3a7dc2e130afdb85c97fae32
Maaz Rafiq <maaz.rafique.75@gmail.com>
2026-02-21 18:43:59 +0200

    add files

---
+++ main.py
@@ -0,0 +1,13 @@
+from query_reformulation.pipeline import LLMQueryProcessingPipeline
+
+query = """
+Hey so I've been kinda feeling weird lately,
+my chest feels tight when I climb stairs
+and I get really tired.
+Not sure if it's serious.
+"""
+
+pipeline = LLMQueryProcessingPipeline()
+result = pipeline.run(query)
+
+print(result)
